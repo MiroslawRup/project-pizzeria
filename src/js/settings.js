7197f56faf0c750e698e278c1c155948726519d5
Mirosław Rup <chrupek999@gmail.com>
2021-04-06 20:15:32 +0200

    test heroku

--- src/js/settings.js
+++ src/js/settings.js
@@ -141,7 +141,7 @@ export const settings = {
   },
 
   db: {
-    url: '//localhost:3131',
+    url: '//' + window.location.hostname + (window.location.hostname=='localhost' ? ':3131' : ''),
     product: 'product',
     order: 'order',
     booking: 'booking',
